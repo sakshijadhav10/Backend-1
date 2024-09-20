ad8fbb5dad90b84dde0fe47af45b22cc38426061
sakshijadhav10 <131511233+sakshijadhav10@users.noreply.github.com>
2024-09-20 13:16:36 +0530

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Should be 'dist' or your desired build directory
+  },
   plugins: [react()],
 
   server: {
     port: 3000,
     proxy: {
       
-      "/api": "https://ecommerce-8-lcx4.onrender.com",
-      "/upload": "https://ecommerce-8-lcx4.onrender.com"
+      "/api": "https://ecommerceapp1-lfnc.onrender.com/",
+      "/upload": "https://ecommerceapp1-lfnc.onrender.com/"
     },
   },})
